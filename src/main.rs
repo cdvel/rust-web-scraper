1cc6ec9c0e6b6075c052e49918a2185aac6b9438
Cesar D. Velandia <cesar@velandia.co>
2020-05-11 21:18:29 +0800

    Collect all links from HN

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,24 @@
+extern crate reqwest;
+extern crate select;
+
+use select::document::Document;
+use select::predicate::Name;
 
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", hacker_news("https://news.ycombinator.com"));
+}
+
+#[tokio::main]
+async fn hacker_news(url: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let response = reqwest::get(url).await?;
+    assert!(response.status().is_success());
+
+    let body = response.text().await?;
+    Document::from_read(body.as_bytes())
+        .unwrap()
+        .find(Name("a"))
+        .filter_map(|n| n.attr("href"))
+        .for_each(|x| println!("{}", x));
+    
+    Ok(())
 }
